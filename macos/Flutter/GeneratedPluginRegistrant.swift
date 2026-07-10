45fb86948da947a696f6a0cd91736b807149a42d
Veil Network <zeroil@tutamail.com>
2026-07-10 16:09:46 +0300

    media: macos pod artifacts for video_player (follow-up to 2442c31)
    GeneratedPluginRegistrant.swift + Podfile.lock pick up
    video_player_avfoundation - generated when the in-chat video playback
    commit added the plugin, missed from that commit.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import screen_retriever_macos
 import shared_preferences_foundation
 import tray_manager
 import url_launcher_macos
+import video_player_avfoundation
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -24,5 +25,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
